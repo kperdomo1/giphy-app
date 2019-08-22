85baf2631a2ce569b5ea2e909a2721d1b52955fd
Kristopher M. Perdomo Ch <kperdomo.ch@gmail.com>
2019-08-21 23:52:41 -0300

    Searchar improval

--- src/components/searchbar/Searchbar.js
+++ src/components/searchbar/Searchbar.js
@@ -7,10 +7,15 @@ import './searchbar.scss';
 const Searchbar = ({ onChange }) => (
   <div className="searchbar-wrapper">
     <div className="input-container">
-      <input onChange={ev => onChange(ev.target.value)} autoFocus={true} type="text" placeholder="Search for gifs!" />
-      <div className="search-icon">
+      <input
+        id="gif-searcher"
+        onChange={ev => onChange(ev.target.value)} autoFocus={true}
+        type="text"
+        placeholder="Search for gifs!"
+      />
+      <label htmlFor="gif-searcher" className="search-icon">
         <SearchIcon />
-      </div>
+      </label>
     </div>
     <a className="powered-by" href="https://giphy.com/" target="_blank" rel="noopener noreferrer">
       <img alt="powered-by-giphy" src="./powred-by.png" />
